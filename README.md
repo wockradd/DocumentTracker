bed24c93ea19eb49b7e4b79f41f1f78e310fa2ff
Lee Donovan <lee.donovan2303@gmail.com>
2020-11-25 23:07:54 +0000

    Task 4 and allowed real big files

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # DocumentTracker
 Document tracker written in Python, CW for F20SC
 
+It'll look for whatever file you specify in the data folder so make sure you save it in the right place
+
 ## Tasks
 - [x] 1
 - [x] 2a
 - [x] 2b
 - [x] 3a
 - [x] 3b
-- [ ] 4
+- [x] 4
 - [ ] 5
 - [ ] 6
 - [ ] 7
 - [ ] 8
 
 ### Notes
-- Does Plotter need to be a class?
+- Update plotter so the title and stuff are correct
 - Task classes in different files?
 - Add Comments
 - Add enums for valid task ids
+- Find better library for task 3
--- cw2.py
+++ cw2.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from tkinter import *
 from tkinter import ttk
 from user_agents import parse
+import time
 
 #useful arguemtns 
 #issuu_cw2.json for file
@@ -28,7 +29,7 @@ continents = {
 cntry_to_cont = { 'AF' : 'AS', 'AX' : 'EU', 'AL' : 'EU', 'DZ' : 'AF', 'AS' : 'OC', 'AD' : 'EU', 'AO' : 'AF', 'AI' : 'NA', 'AQ' : 'AN', 'AG' : 'NA', 'AR' : 'SA', 'AM' : 'AS', 'AW' : 'NA', 'AU' : 'OC', 'AT' : 'EU', 'AZ' : 'AS', 'BS' : 'NA', 'BH' : 'AS', 'BD' : 'AS', 'BB' : 'NA', 'BY' : 'EU', 'BE' : 'EU', 'BZ' : 'NA', 'BJ' : 'AF', 'BM' : 'NA', 'BT' : 'AS', 'BO' : 'SA', 'BQ' : 'NA', 'BA' : 'EU', 'BW' : 'AF', 'BV' : 'AN', 'BR' : 'SA', 'IO' : 'AS', 'VG' : 'NA', 'BN' : 'AS', 'BG' : 'EU', 'BF' : 'AF', 'BI' : 'AF', 'KH' : 'AS', 'CM' : 'AF', 'CA' : 'NA', 'CV' : 'AF', 'KY' : 'NA', 'CF' : 'AF', 'TD' : 'AF', 'CL' : 'SA', 'CN' : 'AS', 'CX' : 'AS', 'CC' : 'AS', 'CO' : 'SA', 'KM' : 'AF', 'CD' : 'AF', 'CG' : 'AF', 'CK' : 'OC', 'CR' : 'NA', 'CI' : 'AF', 'HR' : 'EU', 'CU' : 'NA', 'CW' : 'NA', 'CY' : 'AS', 'CZ' : 'EU', 'DK' : 'EU', 'DJ' : 'AF', 'DM' : 'NA', 'DO' : 'NA', 'EC' : 'SA', 'EG' : 'AF', 'SV' : 'NA', 'GQ' : 'AF', 'ER' : 'AF', 'EE' : 'EU', 'ET' : 'AF', 'FO' : 'EU', 'FK' : 'SA', 'FJ' : 'OC', 'FI' : 'EU', 'FR' : 'EU', 'GF' : 'SA', 'PF' : 'OC', 'TF' : 'AN', 'GA' : 'AF', 'GM' : 'AF', 'GE' : 'AS', 'DE' : 'EU', 'GH' : 'AF', 'GI' : 'EU', 'GR' : 'EU', 'GL' : 'NA', 'GD' : 'NA', 'GP' : 'NA', 'GU' : 'OC', 'GT' : 'NA', 'GG' : 'EU', 'GN' : 'AF', 'GW' : 'AF', 'GY' : 'SA', 'HT' : 'NA', 'HM' : 'AN', 'VA' : 'EU', 'HN' : 'NA', 'HK' : 'AS', 'HU' : 'EU', 'IS' : 'EU', 'IN' : 'AS', 'ID' : 'AS', 'IR' : 'AS', 'IQ' : 'AS', 'IE' : 'EU', 'IM' : 'EU', 'IL' : 'AS', 'IT' : 'EU', 'JM' : 'NA', 'JP' : 'AS', 'JE' : 'EU', 'JO' : 'AS', 'KZ' : 'AS', 'KE' : 'AF', 'KI' : 'OC', 'KP' : 'AS', 'KR' : 'AS', 'KW' : 'AS', 'KG' : 'AS', 'LA' : 'AS', 'LV' : 'EU', 'LB' : 'AS', 'LS' : 'AF', 'LR' : 'AF', 'LY' : 'AF', 'LI' : 'EU', 'LT' : 'EU', 'LU' : 'EU', 'MO' : 'AS', 'MK' : 'EU', 'MG' : 'AF', 'MW' : 'AF', 'MY' : 'AS', 'MV' : 'AS', 'ML' : 'AF', 'MT' : 'EU', 'MH' : 'OC', 'MQ' : 'NA', 'MR' : 'AF', 'MU' : 'AF', 'YT' : 'AF', 'MX' : 'NA', 'FM' : 'OC', 'MD' : 'EU', 'MC' : 'EU', 'MN' : 'AS', 'ME' : 'EU', 'MS' : 'NA', 'MA' : 'AF', 'MZ' : 'AF', 'MM' : 'AS', 'NA' : 'AF', 'NR' : 'OC', 'NP' : 'AS', 'NL' : 'EU', 'NC' : 'OC', 'NZ' : 'OC', 'NI' : 'NA', 'NE' : 'AF', 'NG' : 'AF', 'NU' : 'OC', 'NF' : 'OC', 'MP' : 'OC', 'NO' : 'EU', 'OM' : 'AS', 'PK' : 'AS', 'PW' : 'OC', 'PS' : 'AS', 'PA' : 'NA', 'PG' : 'OC', 'PY' : 'SA', 'PE' : 'SA', 'PH' : 'AS', 'PN' : 'OC', 'PL' : 'EU', 'PT' : 'EU', 'PR' : 'NA', 'QA' : 'AS', 'RE' : 'AF', 'RO' : 'EU', 'RU' : 'EU', 'RW' : 'AF', 'BL' : 'NA', 'SH' : 'AF', 'KN' : 'NA', 'LC' : 'NA', 'MF' : 'NA', 'PM' : 'NA', 'VC' : 'NA', 'WS' : 'OC', 'SM' : 'EU', 'ST' : 'AF', 'SA' : 'AS', 'SN' : 'AF', 'RS' : 'EU', 'SC' : 'AF', 'SL' : 'AF', 'SG' : 'AS', 'SX' : 'NA', 'SK' : 'EU', 'SI' : 'EU', 'SB' : 'OC', 'SO' : 'AF', 'ZA' : 'AF', 'GS' : 'AN', 'SS' : 'AF', 'ES' : 'EU', 'LK' : 'AS', 'SD' : 'AF', 'SR' : 'SA', 'SJ' : 'EU', 'SZ' : 'AF', 'SE' : 'EU', 'CH' : 'EU', 'SY' : 'AS', 'TW' : 'AS', 'TJ' : 'AS', 'TZ' : 'AF', 'TH' : 'AS', 'TL' : 'AS', 'TG' : 'AF', 'TK' : 'OC', 'TO' : 'OC', 'TT' : 'NA', 'TN' : 'AF', 'TR' : 'AS', 'TM' : 'AS', 'TC' : 'NA', 'TV' : 'OC', 'UG' : 'AF', 'UA' : 'EU', 'AE' : 'AS', 'GB' : 'EU', 'US' : 'NA', 'UM' : 'OC', 'VI' : 'NA', 'UY' : 'SA', 'UZ' : 'AS', 'VU' : 'OC', 'VE' : 'SA', 'VN' : 'AS', 'WF' : 'OC', 'EH' : 'AF', 'YE' : 'AS', 'ZM' : 'AF', 'ZW' : 'AF' } 
 
 #list of valid task ids
-validTasks = ["2a","2b","3a","3b","4d","5","6"]
+validTasks = ["2a","2b","3","4","5","6"]
 
 #global vars
 user_uuid = ""
@@ -39,17 +40,38 @@ data = []
 
 
 class DataLoader:
-    global data
     failed = False
+    path = ""
+    fileSize = 0
+    
+    #set the path, see if the file exists, work out how big it is
     def __init__(self,file_name):
-        path = "./data/" + file_name
+        self.path = "./data/" + file_name
         try:
-            with open(path, "r") as JSONfile:
+            with open(self.path, "r") as JSONfile:
+                print("Working out file size")
                 for line in JSONfile:
-                    data.append(json.loads(line))
+                    self.fileSize+=1
         except FileNotFoundError:
             self.failed = True
-            print("Couldnt find the file")
+            print("Couldnt find the file at %s" % self.path)
+
+
+    # x=0 means get the data from 0 - 10%
+    # x=1 means get the data from 10 - 20%
+    # x=2 means get the data from 20 - 30% etc
+    def loadPartOfData(self,x):
+        global data
+        data = []
+        start = (self.fileSize*x)//10
+        end = (self.fileSize*(x+1))//10
+        print("Dealing with lines %d to %d" % (start,end))
+        with open(self.path, "r") as JSONfile:
+            for pos,line in enumerate(JSONfile):
+                if pos >= start and pos < end:
+                     data.append(json.loads(line))
+            
+
 
 
 class Plotter:
@@ -79,12 +101,15 @@ class Task2:
 
     def countCountries(self):
         for x in data:
-            if "subject_doc_id" in x.keys():
+            try:
                 if x["subject_doc_id"] == doc_uuid:
                     if x["visitor_country"] in self.countriesCount:
                         self.countriesCount[x["visitor_country"]] += 1
                     else:
                         self.countriesCount[x["visitor_country"]] = 1
+            except KeyError:
+                pass
+
     
     def countContinents(self):
         for x in self.countriesCount:
@@ -94,19 +119,37 @@ class Task2:
                 self.continentsCount[continents[cntry_to_cont[x]]] = self.countriesCount[x]
 
 
+
 class Task3:
     browsersCount = {}
 
     def countBrowsers(self):
         for x in data:
-            browser = parse(x["visitor_useragent"]).browser.family
-            if browser in self.browsersCount:
-                self.browsersCount[browser] += 1
-            else:
-                self.browsersCount[browser] = 1
+            try:
+                browser = parse(x["visitor_useragent"]).browser.family
+                if browser in self.browsersCount:
+                    self.browsersCount[browser] += 1
+                else:
+                    self.browsersCount[browser] = 1
+            except KeyError:
+                pass
 
 
-            
+
+class Task4:
+    topReaders = {}
+
+    def getTopReaders(self):
+        for x in data:
+            try:
+                if x["visitor_uuid"] in self.topReaders:
+                    self.topReaders[x["visitor_uuid"]] += x["event_readtime"]/60000
+                else:
+                    self.topReaders[x["visitor_uuid"]] = x["event_readtime"]/60000
+            except KeyError:
+                pass
+        self.topReaders = dict(sorted(self.topReaders.items(), key=lambda x:x[1], reverse=True)[:10])
+        
 
 
 
@@ -115,37 +158,53 @@ class Task3:
 def doTask(task_id):
     dataLoader = DataLoader(file_name)
     plotter = Plotter()
-
+    
     #make sure data loaded properly before doing any tasks
     if not dataLoader.failed:
-
+       
         if task_id[0] == "2":
             #need a doc id for task 2
             if doc_uuid == "":
                 print("Please specify a document like this: cw2 -d doc_uuid")
             else: 
-                #needed for both tasks
+                #do this for a and b
                 task2 = Task2()
-                task2.countCountries()
+                for i in range(10):
+                    dataLoader.loadPartOfData(i)
+                    task2.countCountries()
                 
                 if task_id[1] == "b":
-                    #2b
+                    #finish off b
                     task2.countContinents()
                     print(task2.continentsCount)
                     plotter.displayVert(task2.continentsCount)
                 else:
-                    #2a
+                    #finish off a
+                    print(task2.countriesCount)
                     plotter.displayVert(task2.countriesCount)
 
-        if task_id[0] == "3":
+        if task_id == "3":
             task3 = Task3()
-            task3.countBrowsers()
+            for i in range(10):
+                dataLoader.loadPartOfData(i)
+                task3.countBrowsers()
+            print(task3.browsersCount)
             plotter.displayHorz(task3.browsersCount)
+
+        if task_id == "4":
+            task4 = Task4()
+            for i in range(10):
+                dataLoader.loadPartOfData(i)
+                task4.getTopReaders()
+            print(task4.topReaders)
+            plotter.displayHorz(task4.topReaders)
 
 
 
 
 def main():
+    startTime = time.time()
+
     global user_uuid
     global doc_uuid
     global task_id
@@ -173,6 +232,9 @@ def main():
             print(validTasks)
         else:
             doTask(task_id)
+    
+    endTime = time.time() - startTime
+    print("Time taken: %fs" % endTime)
 
 
 if __name__ == "__main__":
